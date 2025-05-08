370352ad068d3cca5769393b05ba760729e9e252
Brian Pile <brian.pile@gmail.com>
2025-05-08 18:19:21 +0800

    add load_iv function

---
+++ R/load_iv.R
@@ -0,0 +1,59 @@
+#' Load IV files
+#'
+#' @param file Path to the IV file
+#'
+#' @returns A data frame
+#' @export
+#'
+#' @importFrom rlang .data
+#'
+#' @examples
+#' filepath <- system.file("extdata", "25FC01497_CH1.csv", package = "loadr")
+#' if (file.exists(filepath)) {
+#'   df <- load_iv(filepath)
+#'   head(df)
+#' }
+
+load_iv = function(file) {
+
+  # determine the test station (unclear from file names / content)
+  test_station = NA
+
+  # extract dut info from file name
+  work_order = stringr::str_extract(file, "WO\\d{2}-\\d{4}")
+  fc_id = stringr::str_extract(file, "\\d{2}FC\\d{4,5}")
+  ch = stringr::str_extract(file, "CH([1-4])", group = 1)
+  dut_id = paste(sep = "-", fc_id, ch)
+  test_id = stringr::str_extract(file, "_(\\d{2})_LIV", group = 1)
+  temperature = stringr::str_extract(file, "_(\\d{2})[.]?\\dC?", group = 1)
+
+  # read data
+  df = data.table::fread(
+    file = file,
+    skip = 1
+  ) |>
+    tibble::as_tibble()
+
+  # select and rename columns
+  df = df |>
+    dplyr::select(
+      current = .data$Ianodf,
+      voltage = .data$Vanodf,
+    )
+
+  # add dut info
+  df = df |>
+    dplyr::mutate(
+      work_order = work_order,
+      test_station = test_station,
+      fc_id = fc_id,
+      ch = ch,
+      dut_id = dut_id,
+      test_id = test_id,
+      temperature = temperature,
+      .before = .data$current
+    )
+
+  return(df)
+
+}
